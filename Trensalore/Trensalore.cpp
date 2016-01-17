b49e540ae30df5bf08cd1c557824ec4c8d427118
andre.s.bonner <andre.s.bonner>
2016-01-17 01:02:55 -0500

    PaintHook

--- Trensalore/Trensalore.cpp
+++ Trensalore/Trensalore.cpp
@@ -19,6 +19,12 @@ BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK	Find(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	Replace(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	Goto(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK PaintHook(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+BOOL LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName);
+BOOL SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName);
 
 int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -136,7 +142,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	static HWND hEdit, hStatus;
 	RECT rcStatus, rcClient;
 	int iStatusHeight, iEditHeight, iStatusWidths[] = { 100, 200, -1 };
-	char szFileName[MAX_PATH];
+	char szFileName[MAX_PATH] = "";
+	SYSTEMTIME tmeCurrent;
 
 	switch (message)
 	{
@@ -164,7 +171,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		SendMessage(hEdit,
 			WM_SETTEXT,
 			NULL,
-			(LPARAM)L"Insert text here...");
+			(LPARAM)"Insert text here...");
 		// Create an statusbar
 		hStatus = CreateWindowEx(0,
 			STATUSCLASSNAME,
@@ -179,13 +186,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			GetModuleHandle(NULL),
 			NULL);
 		if (!hStatus){
-			MessageBox(hWnd, L"Failed to Create StatusBar", L"Statusbar Creation", MB_OK | MB_ICONEXCLAMATION);
+			MessageBox(hWnd, "Failed to Create StatusBar", "Statusbar Creation", MB_OK | MB_ICONEXCLAMATION);
 			return FALSE;
 		}
 
 		SendMessage(hStatus, SB_SETPARTS, sizeof(iStatusWidths) / sizeof(int), (LPARAM)iStatusWidths);
-		SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)L"Hi there :)");
-		
+		SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)"Hi there :)");
+
+		SetWindowText(hWnd, "Untitled - Trensalore");
 	}
 	break;
 	case WM_COMMAND:
@@ -196,43 +204,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 		case ID_FILE_NEW:
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+
+			SetWindowText(hWnd, "Untitled - Trensalore");
 			break;
 		case ID_FILE_OPEN:
 			//Open Dialog
 			OPENFILENAME openFileDialog;
-			
+
 			ZeroMemory(&openFileDialog, sizeof(openFileDialog));
 			openFileDialog.lStructSize = sizeof(openFileDialog);
 			openFileDialog.hwndOwner = hWnd;
-			openFileDialog.lpstrFilter = L"Angelic Text (*.Atxt)\0*.Dtxt\0Text Files (*.txt)\0*txt\0All Files (*.*)\0*.*\0";
-			openFileDialog.lpstrFile = (LPWSTR)szFileName;
+			openFileDialog.lpstrFilter = "Angelic Text (*.Atxt)\0*.Atxt\0Text Files (*.txt)\0*txt\0All Files (*.*)\0*.*\0";
+			openFileDialog.lpstrFile = szFileName;
 			openFileDialog.nMaxFile = MAX_PATH;
 			openFileDialog.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
-			openFileDialog.lpstrDefExt = L"Atxt";
+			openFileDialog.lpstrDefExt = "Atxt";
 
 			if (GetOpenFileName(&openFileDialog)){
-				MessageBox(0, (LPWSTR)szFileName, _T("File Name"),
+				MessageBox(0, szFileName, _T("File Name"),
 					MB_OK | MB_ICONINFORMATION);
+
+				LoadTextFileToEdit(hEdit, szFileName);
+				SetWindowText(hWnd, lstrcat(szFileName, " - Trensalore"));
 
 			}
 			break;
 		case ID_FILE_SAVEAS:
 			//Save Dialog
 			OPENFILENAME saveFileDialog;
-			
+
 			ZeroMemory(&saveFileDialog, sizeof(saveFileDialog));
 			saveFileDialog.lStructSize = sizeof(saveFileDialog);
 			saveFileDialog.hwndOwner = hWnd;
-			saveFileDialog.lpstrFilter = L"Angelic Text (*.Atxt)\0*.Dtxt\0Text Files (*.txt)\0*txt\0All Files (*.*)\0*.*\0";
-			saveFileDialog.lpstrFile = (LPWSTR)szFileName;
+			saveFileDialog.lpstrFilter = "Angelic Text (*.Atxt)\0*.Atxt\0Text Files (*.txt)\0*txt\0All Files (*.*)\0*.*\0";
+			saveFileDialog.lpstrFile = szFileName;
 			saveFileDialog.nMaxFile = MAX_PATH;
 			saveFileDialog.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
-			saveFileDialog.lpstrDefExt = L"Atxt";
+			saveFileDialog.lpstrDefExt = "Atxt";
 
 			if (GetSaveFileName(&saveFileDialog)){
-				MessageBox(0, (LPWSTR)szFileName, _T("File Name"),
+				MessageBox(0, szFileName, _T("File Name"),
 					MB_OK | MB_ICONINFORMATION);
 
+				SaveTextFileFromEdit(hEdit, szFileName);
+				SetWindowText(hWnd, lstrcat(szFileName, " - Trensalore"));
 			}
 			break;
 		case ID_FILE_SAVE:
@@ -253,7 +268,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			psd.rtMargin.left = 1250;
 			psd.rtMargin.right = 1250;
 			psd.rtMargin.bottom = 1000;
-			psd.lpfnPagePaintHook = NULL;
+			psd.lpfnPagePaintHook = PaintHook;
 
 			if (PageSetupDlg(&psd) == TRUE)
 			{
@@ -307,18 +322,70 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_FIND), hWnd, Find);
 			break;
 		case ID_EDIT_FINDNEXT:
+			FINDREPLACE fr;       // common dialog box structure
+			//HWND hwnd;            // owner window
+			CHAR szFindWhat[80];  // buffer receiving string
+			HWND hdlg;
+			hdlg = NULL;     // handle to Find dialog box
+
+			// Initialize FINDREPLACE
+			ZeroMemory(&fr, sizeof(fr));
+			fr.lStructSize = sizeof(fr);
+			fr.hwndOwner = hWnd;
+			fr.lpstrFindWhat = szFindWhat;
+			fr.wFindWhatLen = 80;
+			fr.Flags = 0;
+
+			hdlg = FindText(&fr);
+
 			break;
 		case ID_EDIT_REPLACE:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_REPLACE), hWnd, Replace);
 			break;
 		case ID_EDIT_GOTO:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_GOTO), hWnd, Goto);
 			break;
 		case ID_EDIT_SELECTALL:
+			SendMessage(hEdit, EM_SETSEL, 0, -1);
 			break;
 		case ID_EDIT_TIME:
+			TCHAR str[20], str1[20];
+			GetTimeFormat(
+				MAKELCID(LANG_USER_DEFAULT, SORT_DEFAULT),
+				0,
+				&tmeCurrent,
+				NULL,
+				str,
+				20);
+
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)str);
 			break;
 		case ID_FORMAT_WORDWRAP:
 			break;
 		case ID_FORMAT_FONT:
+			CHOOSEFONT cf;            // common dialog box structure
+			static LOGFONT lf;        // logical font structure
+			static DWORD rgbCurrent;  // current text color
+			HFONT hfont, hfontPrev;
+			DWORD rgbPrev;
+
+			// Initialize CHOOSEFONT
+			ZeroMemory(&cf, sizeof(cf));
+			cf.lStructSize = sizeof(cf);
+			cf.hwndOwner = hWnd;
+			cf.lpLogFont = &lf;
+			cf.rgbColors = rgbCurrent;
+			cf.Flags = CF_SCREENFONTS | CF_EFFECTS;
+
+			if (ChooseFont(&cf) == TRUE)
+			{
+				//hfont = CreateFontIndirect(cf.lpLogFont);
+				//hfontPrev = (HFONT)SelectObject(hdc, hfont);
+				//rgbCurrent = cf.rgbColors;
+				//rgbPrev = SetTextColor(hdc, rgbCurrent);
+
+			}
+
 			break;
 		case ID_VIEW_STATUSBAR:
 			break;
@@ -406,4 +473,148 @@ INT_PTR CALLBACK Find(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	}
 	return (INT_PTR)FALSE;
+}
+
+// Message handler for replace box.
+INT_PTR CALLBACK Replace(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+// Message handler for goto box.
+INT_PTR CALLBACK Goto(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+BOOL LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName)
+{
+	HANDLE hFile;
+	BOOL bSuccess = FALSE;
+
+	hFile = CreateFile(pszFileName, GENERIC_READ, FILE_SHARE_READ, NULL,
+		OPEN_EXISTING, 0, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		DWORD dwFileSize;
+
+		dwFileSize = GetFileSize(hFile, NULL);
+		if (dwFileSize != 0xFFFFFFFF)
+		{
+			LPSTR pszFileText;
+
+			pszFileText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
+			if (pszFileText != NULL)
+			{
+				DWORD dwRead;
+
+				if (ReadFile(hFile, pszFileText, dwFileSize, &dwRead, NULL))
+				{
+					pszFileText[dwFileSize] = 0; // Add null terminator
+					if (SetWindowText(hEdit, pszFileText))
+						bSuccess = TRUE; // It worked!
+				}
+				GlobalFree(pszFileText);
+			}
+		}
+		CloseHandle(hFile);
+	}
+	return bSuccess;
+}
+
+BOOL SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName)
+{
+	HANDLE hFile;
+	BOOL bSuccess = FALSE;
+
+	hFile = CreateFile(pszFileName, GENERIC_WRITE, 0, NULL,
+		CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		DWORD dwTextLength;
+
+		dwTextLength = GetWindowTextLength(hEdit);
+		// No need to bother if there's no text.
+		if (dwTextLength > 0)
+		{
+			LPSTR pszText;
+			DWORD dwBufferSize = dwTextLength + 1;
+
+			pszText = (LPSTR)GlobalAlloc(GPTR, dwBufferSize);
+			if (pszText != NULL)
+			{
+				if (GetWindowText(hEdit, pszText, dwBufferSize))
+				{
+					DWORD dwWritten;
+
+					if (WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))
+						bSuccess = TRUE;
+				}
+				GlobalFree(pszText);
+			}
+		}
+		CloseHandle(hFile);
+	}
+	return bSuccess;
+}
+
+BOOL CALLBACK PaintHook(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	LPRECT lprc;
+	COLORREF crMargRect;
+	HDC hdc, hdcOld;
+
+	switch (uMsg)
+	{
+		// Draw the margin rectangle. 
+	case WM_PSD_MARGINRECT:
+		hdc = (HDC)wParam;
+		lprc = (LPRECT)lParam;
+
+		// Get the system highlight color. 
+		crMargRect = GetSysColor(COLOR_HIGHLIGHT);
+
+		// Create a dash-dot pen of the system highlight color and 
+		// select it into the DC of the sample page. 
+		hdcOld = (HDC)SelectObject(hdc, CreatePen(PS_DASHDOT, .5, crMargRect));
+
+		// Draw the margin rectangle. 
+		Rectangle(hdc, lprc->left, lprc->top, lprc->right, lprc->bottom);
+
+		// Restore the previous pen to the DC. 
+		SelectObject(hdc, hdcOld);
+		return TRUE;
+
+	default:
+		return FALSE;
+	}
+	return TRUE;
 }
